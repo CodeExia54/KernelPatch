6b2595d766b9273c026062eda9ccdd766a7d95b0
CodeExia54 <codeexia97@gmail.com>
2024-11-02 10:09:35 +0530

    Update kpmodule.h

--- kernel/include/kpmodule.h
+++ kernel/include/kpmodule.h
@@ -6,11 +6,7 @@
 #ifndef _KP_KPMODULE_H_
 #define _KP_KPMODULE_H_
 
-#define KPM_INFO(name, info, limit)                                 \
-    _Static_assert(sizeof(info) <= limit, "Info string too long");  \
-    static const char __kpm_info_##name[] __attribute__((__used__)) \
-    __attribute__((section(".kpm.info"), unused, aligned(1))) = #name "=" info
-
+// Define maximum lengths for module metadata
 #define KPM_NAME_LEN 32
 #define KPM_VERSION_LEN 12
 #define KPM_LICENSE_LEN 32
@@ -18,17 +14,26 @@
 #define KPM_DESCRIPTION_LEN 512
 #define KPM_ARGS_LEN 1024
 
+// Macro to define module information with a static assertion
+#define KPM_INFO(name, info, limit)                                     \
+    _Static_assert(sizeof(info) <= limit, "Info string too long");    \
+    static const char __kpm_info_##name[] __attribute__((__used__))   \
+    __attribute__((section(".kpm.info"), unused, aligned(1))) = #name "=" info
+
+// Macros for defining module metadata
 #define KPM_NAME(x) KPM_INFO(name, x, KPM_NAME_LEN)
 #define KPM_VERSION(x) KPM_INFO(version, x, KPM_VERSION_LEN)
 #define KPM_LICENSE(x) KPM_INFO(license, x, KPM_LICENSE_LEN)
 #define KPM_AUTHOR(x) KPM_INFO(author, x, KPM_AUTHOR_LEN)
 #define KPM_DESCRIPTION(x) KPM_INFO(description, x, KPM_DESCRIPTION_LEN)
 
+// Function pointer types for module operations
 typedef long (*mod_initcall_t)(const char *args, const char *event, void *reserved);
 typedef long (*mod_ctl0call_t)(const char *ctl_args, char *__user out_msg, int outlen);
 typedef long (*mod_ctl1call_t)(void *a1, void *a2, void *a3);
 typedef long (*mod_exitcall_t)(void *reserved);
 
+// Macros for registering module callbacks
 #define KPM_INIT(fn) \
     static mod_initcall_t __kpm_initcall_##fn __attribute__((__used__)) __attribute__((__section__(".kpm.init"))) = fn
 
@@ -41,4 +46,4 @@ typedef long (*mod_exitcall_t)(void *reserved);
 #define KPM_EXIT(fn) \
     static mod_exitcall_t __kpm_exitcall_##fn __attribute__((__used__)) __attribute__((__section__(".kpm.exit"))) = fn
 
-#endif
+#endif // _KP_KPMODULE_H_
